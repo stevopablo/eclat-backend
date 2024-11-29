36be69e63a606369fc652a7dad5dcce470f1fe2e
stevopablo <stevaopabllo@gmai.com>
2024-11-28 21:56:29 -0300

    change to mysql

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,17 +1,25 @@
-import { MongoClient } from 'mongodb';
+// import { MongoClient } from 'mongodb';
 
-export default async function conectarAoBanco(stringConexao) {
-    let mongoClient;
+// export default async function conectarAoBanco(stringConexao) {
+//     let mongoClient;
 
-    try {
-        mongoClient = new MongoClient(stringConexao);
-        console.log('Conectando ao cluster do banco de dados...');
-        await mongoClient.connect();
-        console.log('Conectado ao MongoDB Atlas com sucesso!');
+//     try {
+//         mongoClient = new MongoClient(stringConexao);
+//         console.log('Conectando ao cluster do banco de dados...');
+//         await mongoClient.connect();
+//         console.log('Conectado ao MongoDB Atlas com sucesso!');
 
-        return mongoClient;
-    } catch (erro) {
-        console.error('Falha na conexão com o banco!', erro);
-        process.exit();
-    }
-}
+//         return mongoClient;
+//     } catch (erro) {
+//         console.error('Falha na conexão com o banco!', erro);
+//         process.exit();
+//     }
+// }
+import { Sequelize } from 'sequelize';
+
+const sequelize = new Sequelize('eclat_database', 'root', 'root', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+export default sequelize;
